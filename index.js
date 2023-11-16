f306b948aa62ecab33e5734ed057e738071a42c4
Espazam <wesleymmenezes@hotmail.com>
2023-11-16 00:14:44 -0300

    Quase Finalizando

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 let nickname = "Espazam"
-let XP = 100
+let XP = 
 
 if(XP < 1000){
 console.log ("Ferro");
-} else {
-  (XP >= 1001 && XP <= 2000)
+} else if (XP >= 1001 && XP <= 2000) {
   console.log("Bronze")
+} else if (XP >= 2001 && XP <= 5000) {
+    console.log("Prata")
+} else if (XP >= 6.001 && XP <= 7000) {
+    console.log("ouro")
+} else if (XP >= 7.001 && XP <= 8000) {
+    console.log("Platina")
+} else if (XP >= 8.001 && XP <= 10.000) {
+    console.log("Imortal")
+} else if (XP >= 10.001) {
+    console.log("Radiante")
 }
